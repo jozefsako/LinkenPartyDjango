c275b21792850c514046a1c0cf2047e5635a0af1
jozef_sako <jozef.sako@student.vinci.be>
2019-05-20 17:38:27 +0200

    feat : connexion

--- polls/views.py
+++ polls/views.py
@@ -86,13 +86,16 @@ class UserViewSet(viewsets.ModelViewSet):
 # UserList
 class UserList(APIView):
     def post(self, request, format=None):
-        serializer = AUserSerializerWithToken(data=request.data)
-        #serializer = UserSerializer(data=request.data)
-        print(serializer)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        username = request.data['username']
+        password = request.data['password']
+        aUser = AUsers.objects.filter(username=username)
+        aUser_serialized = serializers.serialize('json', aUser)
+        aUser_str = str(aUser_serialized)
+        formated_user = aUser_str.replace('\'', '\"')
+        jsonUser = json.loads(formated_user)
+        if(jsonUser[0]['fields']['password'] == password):
+             return Response(json.loads(formated_user), status=status.HTTP_202_ACCEPTED)
+        return Response(status=status.HTTP_400_BAD_REQUEST)
 
 
 # EventList
